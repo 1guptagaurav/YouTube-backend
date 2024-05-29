378a7a77b8a88c84901da3c02d338a80d43b638b
Gaurav Gupta <gauravgupta1197@gmail.com>
2024-05-29 16:34:01 +0530

    commilt all

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,8 @@
 import { asyncHandler } from "/home/guptagaurav/Backend/Video 7/src/utils/asyncHandler";
 
 
-const registerUser= asyncHandler(async (req,res)=>{
+const registerUser= asyncHandler(
+    async (req,res)=>{
     res.status(200).json({
         message:"ok"
     })
